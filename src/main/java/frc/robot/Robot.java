b29e2b50946170758d435e59787b0c808cbb321a
Aries Powvalla <aries@ninjam.co>
2025-01-08 17:50:37 -0800

    Drivetrain and controllers Basic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,61 +5,71 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each
- * mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the
- * package after creating
- * this project, you must also update the Main.java file in the project.
- */
 public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
 
-    public Robot() {
+  private final Core m_robotContainer;
 
+  public Robot() {
+    m_robotContainer = new Core();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run(); 
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
+  }
 
-    @Override
-    public void robotPeriodic() {
-    }
+  @Override
+  public void autonomousPeriodic() {}
 
-    @Override
-    public void autonomousInit() {
-    }
+  @Override
+  public void autonomousExit() {}
 
-    @Override
-    public void autonomousPeriodic() {
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
+  }
 
-    @Override
-    public void teleopInit() {
-    }
+  @Override
+  public void teleopPeriodic() {}
 
-    @Override
-    public void teleopPeriodic() {
-    }
+  @Override
+  public void teleopExit() {}
 
-    @Override
-    public void disabledInit() {
-    }
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-    @Override
-    public void disabledPeriodic() {
-    }
+  @Override
+  public void testPeriodic() {}
 
-    @Override
-    public void testInit() {
-    }
+  @Override
+  public void testExit() {}
 
-    @Override
-    public void testPeriodic() {
-    }
-
-    @Override
-    public void simulationInit() {
-    }
-
-    @Override
-    public void simulationPeriodic() {
-    }
+  @Override
+  public void simulationPeriodic() {}
 }
