df9f302783ce47133df0b95119c003c69f93e4f9
jkhiramatsu <jhiramat28@jesuitmail.org>
2025-01-25 12:39:24 -0800

    Update ElevatorSubsystem.java
    elevator config

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,7 +4,12 @@ import java.io.ObjectInputFilter.Config;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.CustomParamsConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.ConnectedMotorValue;
 
@@ -16,6 +21,9 @@ public class ElevatorSubsystem {
     public static TalonFX elevatorMotor1;
     public static TalonFX elevatorMotor2;
     public static CANrange range;
+    public CANcoderConfiguration coderConfig = new CANcoderConfiguration();
+    private CANrangeConfiguration rangeConfig = new CANrangeConfiguration();
+
     CANcoder beltPosition = new CANcoder(Constants.beltPosition_ID);
 
     DigitalInput L1Bottom = new DigitalInput(0);
@@ -25,11 +33,15 @@ public class ElevatorSubsystem {
     DigitalInput L3Bottom = new DigitalInput(4);
     DigitalInput L3Top = new DigitalInput(5);
 
+    public CANcoderConfiguration withMagnetSensor(MagnetSensorConfigs newMagnetSensor) {
+    }
+
     //private final double elevatorSpeed = 0.250;  -> find appropriate value
 
     public ElevatorSubsystem() {
         elevatorMotor1 = new TalonFX(0); // TODO
         elevatorMotor2 = new TalonFX(0); // TODO
+        coderConfig = new CANcoderConfiguration();
     }
 
     public void startElevatorUp() {
