a1c0d7e21d87c2789a6630798e532d146d962e09
Ethan Paes <epaes27@jesuitmail.org>
2025-08-28 17:29:29 -0700

    Manipulator Changes
    - Added all methods for the API
    - Added default RPMs and threshold (needs to be tuned)
    - Removed old methods
    - Worked on the auto-stop features
    - TODO: Speed control (using max speed) and overriding (if necessary)
    
    - Ethan & Xan
    
    Co-Authored-By: PastaXan <181164286+PastaXan@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -4,8 +4,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.hardware.core.CoreCANrange;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -14,150 +12,134 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ManipulatorSubsystem extends SubsystemBase {
 
+    private enum State {
+        INTAKING,
+        OUTTAKING,
+        LOADED,
+        EMPTY
+    }
+
     public CoreCANrange sensor;
     public TalonFX control;
-    // public SparkMax eject;
 
-    private boolean isHolding = false;
+    private State currentState;
 
-    boolean algaeIntake = false;
-
-    private boolean isIntaking = false;
-    public boolean isOuttaking = false;
-
-    public boolean overriding = false;
-    public boolean allowMaxOuttake = false;
+    private final double defaultIntakeRPM = 60;
+    private final double defaultOuttakeRPM = 60;
+    private final double distanceThreshold = 0.1;
 
     public ManipulatorSubsystem() {
 
-        // this.eject = new SparkMax(33, MotorType.kBrushless);
         this.control = new TalonFX(26, "FastFD");
         this.sensor = new CoreCANrange(27, "FastFD");
-
-        SparkMaxConfig config = new SparkMaxConfig();
-        config.idleMode(IdleMode.kBrake);
-        config.signals.primaryEncoderPositionPeriodMs(5);
-        // eject.configure(config, ResetMode.kResetSafeParameters,
-        // PersistMode.kPersistParameters);
 
         control.setNeutralMode(NeutralModeValue.Brake);
     }
 
-    public void intake() {
-        isIntaking = true;
-        isOuttaking = false;
-        control.set(-0.75);
+    public SequentialCommandGroup intake() {
+        return intake(defaultIntakeRPM);
     }
 
-    public void intake(double speed) {
-        System.out.println("INTAKING DKFK");
-        isIntaking = true;
-        isOuttaking = false;
-        control.set(speed);
-    }
-
-    public void outtake() {
-        isIntaking = false;
-        isOuttaking = true;
-        control.set(1.0);
-    }
-
-    public void outtake(double speed) {
-        System.out.println("OUTTAKING DKFK");
-        isIntaking = false;
-        isOuttaking = true;
-        control.set(speed);
-    }
-
-    public void stopOuttake() {
-        System.out.println("STOPPING OUTTAKING");
-        isIntaking = false;
-        isOuttaking = false;
-        control.set(0);
-    }
-
-    public void spinAt(double speed) {
-        control.set(-speed);
-
-        if (speed > 0.0) {
-            isIntaking = false;
-            isOuttaking = true;
-        } else {
-            isIntaking = true;
-            isOuttaking = false;
-        }
-    }
-
-    public void stop() {
-        isIntaking = false;
-        isOuttaking = false;
-        control.stopMotor();
-    }
-
-    public boolean getIsIntaking() {
-        return isIntaking;
-    }
-
-    int algaeClock = 0;
-
-    public void holdAlgae() {
-        algaeIntake = !algaeIntake;
-    }
-
-    public void setOverride(boolean x) {
-        overriding = x;
-    }
-
-    public SequentialCommandGroup OutakeSpeed(double setspeed) {
+    public SequentialCommandGroup intake(double rpm) {
         SequentialCommandGroup group = new SequentialCommandGroup();
-        Command c = new InstantCommand(() -> this.outtake(setspeed));
+        Command c = new InstantCommand(() -> runIntake(rpm));
         group.addCommands(c);
 
         return group;
-    
-
     }
 
-    public SequentialCommandGroup IntakeSpeed(double setspeed) {
+    public SequentialCommandGroup outtake() {
+        return outtake(defaultOuttakeRPM);
+    }
+
+    public SequentialCommandGroup outtake(double rpm) {
         SequentialCommandGroup group = new SequentialCommandGroup();
-        Command c = new InstantCommand(() -> this.intake(setspeed));
+        Command c = new InstantCommand(() -> runOuttake(rpm));
         group.addCommands(c);
 
         return group;
-    
-
     }
 
-    int clock = 11;
+    private void runOuttake(double rpm) {
+        control.set(rpm); // TODO: FIGURE OUT MATH FOR THIS, should be Normalrpm/Maxrpm
+    }
+
+    private void runIntake(double rpm) {
+        control.set(rpm); // TODO: FIGURE OUT MATH FOR THIS, should be Normalrpm/Maxrpm
+    }
+
+    public boolean isGamepiecePresent() {
+        return sensor.getDistance().getValueAsDouble() <= distanceThreshold;
+    }
+
+    public State mechanismStatus() { // INTAKING, OUTTAKING, LOADED, EMPTY
+        return currentState;
+    }
+
+    public double getDefaultIntakeRpm() {
+        return defaultIntakeRPM;
+    }
+
+    public double getDefaultOuttakeRpm() {
+        return defaultOuttakeRPM;
+    }
+
+    // public SequentialCommandGroup intake(double setspeed) {
+    // SequentialCommandGroup group = new SequentialCommandGroup();
+    // Command c = new InstantCommand(() -> runIntake(setspeed));
+    // group.addCommands(c);
+
+    // return group;
+    // }
+
+    // public SequentialCommandGroup outtake(double setspeed) {
+    // SequentialCommandGroup group = new SequentialCommandGroup();
+    // Command c = new InstantCommand(() -> runOuttake(setspeed));
+    // group.addCommands(c);
+
+    // return group;
+    // }
+
+    // public SequentialCommandGroup intake() {
+    // return this.intake(defaultSpeed);
+    // }
+
+    // public SequentialCommandGroup outtake() {
+    // return this.intake(defaultSpeed);
+    // }
+
+    // private void runIntake(double speed) {
+    // if (isHolding) {
+    // return;
+    // }
+
+    // control.set(speed);
+
+    // isIntaking = true;
+    // }
+
+    // private void runOuttake(double speed) {
+    // if (!isHolding) {
+    // return;
+    // }
+
+    // control.set(speed);
+
+    // isOuttaking = false;
+    // }
+
+    // private void stop() {
+    // control.set(0);
+    // }
+
+    // // public void setOverride(boolean x) {
+    // // overriding = x;
+    // // }
+
+    int clock = 0;
 
     @Override
     public void periodic() {
-
-        clock++;
-        algaeClock++;
-
-        boolean withinRange = sensor.getDistance().getValueAsDouble() <= 0.06
-                && sensor.getIsDetected().getValueAsDouble() == 1.0;
-
-        if (clock > 10 && withinRange) {
-            clock = 5;
-        }
-
-        if (!overriding && isIntaking && !isOuttaking && withinRange && clock == 10 && !algaeIntake) {
-            // stop();
-            isIntaking = false;
-        }
-
-        if (algaeIntake && algaeClock == 20) {
-            // spinAt(0.6);
-        }
-
-        if (algaeClock == 27) {
-            algaeClock = 0;
-            if (!overriding && algaeIntake) {
-                // stop();
-            }
-        }
 
         // if (algaeIntake && clock == 12) {
         // intake();
